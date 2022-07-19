61e56dcba8906ec8a4362bf08907865a548b7f49
zzzjjj <zjzjzjzj1874@gmail.com>
2022-07-19 16:54:50 +0800

    feat: add a simple example for echo
    Signed-off-by: zzzjjj <zjzjzjzj1874@gmail.com>

---
+++ example/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+	e.Logger.Fatal(e.Start(":13123"))
+}
